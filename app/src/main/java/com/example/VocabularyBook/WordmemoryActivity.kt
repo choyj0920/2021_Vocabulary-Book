4eef5cb53c43b8c42e8097114809a7beb85aa077
choyj0920 <44551544+choyj0920@users.noreply.github.com>
2021-02-26 09:34:36 +0900

    import tag-sphere ,minsdk 수정

--- app/src/main/java/com/example/VocabularyBook/WordmemoryActivity.kt
+++ app/src/main/java/com/example/VocabularyBook/WordmemoryActivity.kt
@@ -1,13 +1,33 @@
 package com.example.VocabularyBook
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.TextPaint
 import androidx.appcompat.app.AppCompatActivity
+import com.magicgoop.tagsphere.item.TextTagItem
+import kotlinx.android.synthetic.main.activity_wordmemory.*
 
 class WordmemoryActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wordmemory)
 
+
+        (0..40).map {
+            TextTagItem(text = "Some text $it")
+        }.toList().let {
+            tagView.addTagList(it)
+        }
+
+        tagView.setTextPaint(
+                TextPaint().apply {
+                    isAntiAlias = true
+                    textSize = resources.getDimension(R.dimen.tag_text_size)
+                    color = Color.DKGRAY
+                }
+        )
+
+        
 
 
     }
